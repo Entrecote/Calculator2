b799ac0ffd162d0db71ab9bf398bf5b6d34c22f8
Entrecote <antstepet@gmail.com>
2014-11-30 02:53:25 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,83 +1,142 @@
 #include <iostream>
 #include <string>
 using namespace std;
+//Some func to calculating
+int pl (int sum, int v ) { return  v + sum;}
+int mi (int sum, int v ) { return v - sum;}
+int ma (int sum, int v ) { return v * sum;}
 
-
-
+//The main
 int main()
 {
-    cout << "#############################################" << endl;
-    cout << "###      Calculator Super Nova v. 0,5     ###" << endl;
-    cout << "###         Made in Iceland 2014          ###" << endl;
-    cout << "###   Copyright: Entrecote @github.com    ###" << endl;
-    cout << "###     contact: antstepet@gmail.com      ###" << endl;
-    cout << "#############################################" << endl;
-    cout << "\n\n";
-    cout << "This is private area!\n" << endl;
-    cout << "Please log in" << endl;
+//Strings, Int etc...
 
-    string username;
-    string password;
-    int values;
-    string dothis;
-    string sum;
+        string username;
+        string password;
+        int values = 0;
+        string dothis;
+        int sum = 0;
+        int result =0;
+        int v = 0;
 
-
-    do
+//Startup Window
+        cout << "#############################################" << endl;
+        cout << "###      Calculator Super Nova v. 0,5     ###" << endl;
+        cout << "###         Made in Iceland 2014          ###" << endl;
+        cout << "###   Copyright: Entrecote @github.com    ###" << endl;
+        cout << "###     contact: antstepet@gmail.com      ###" << endl;
+        cout << "#############################################" << endl;
+        cout << "###       This is Private calculator       ##" << endl;
+        cout << "#############################################" << endl;
+        cout << "###            Please log in              ###" << endl;
+        cout << "#############################################" << endl;
+//Login Area
+        do
             {
-                if (username != password && password != username )
+                if (username != "" && password != "" )
                     {
-                    cout << "Wrong username or passsword, try this: \n USERNAME: dovy \n PASSWORD: flottur \n";
+                    //If wrong username or password, error pop up.
+                        cout << "#############################################" << endl;
+                        cout << "###   Error: Wrong username OR password   ###" << endl;
+                        cout << "#############################################" << endl;
+                        cout << "###             Please try again          ###" << endl;
+                        cout << "#############################################" << endl;
+                        cout << "###       May can this account works      ###" << endl;
+                        cout << "###    USERNAME: dovy  PASSWORD: master   ###" << endl;
+                        cout << "#############################################" << endl;
                     }
-                cout << "Username: ";
-                getline(cin, username, '\n');
+                        cout << " \tUsername: ";
+                            getline(cin, username, '\n');
+                        cout << "\tPassword: ";
+                            getline(cin, password, '\n');
+            }
+                //Greetings!
+                while ( username != "dovy" && password != "master");
+                        cout << "#############################################" << endl;
+                        cout << "###                                       ###" << endl;
+                        cout << "###                                       ###" << endl;
+                        cout << "###       Welcome back, Dovydas           ###" << endl;
+                        cout << "###                                       ###" << endl;
+                        cout << "###                                       ###" << endl;
+                        cout << "#############################################" << endl;
 
-                cout << "Password: ";
-                getline(cin, password, '\n');
+                //So you want to calucating some stuff?
+                        cout << "###                                       ###" << endl;
+                        cout << "###                                       ###" << endl;
+                        cout << "###        How many values do you         ###" << endl;
+                        cout << "###          need to calculate?           ###" << endl;
+                        cout << "###                                       ###" << endl;
+                        cout << "###      Select middle 2 - unlimited      ###" << endl;
+                        cout << "###                                       ###" << endl;
+                        cout << "#############################################" << endl;
+                        // Select needed values
+                        cout << "\t I need: ";
+                        cin >> values;
+                        //Going on
+                        cout << "#############################################" << endl;
+                        cout << "###                                       ###" << endl;
+                        cout << "###        You selected " << values << " values          ###" << endl;
+                        cout << "###                                       ###" << endl;
+                        cout << "#############################################" << endl;
 
 
+                for ( int i = 0; i < values; i++ )
+                    {
 
-            }  while ( username != "dovy" && password != "flottur");
-                cout << "Welcome Dovydas, you'r logged in now\n";
+                        //cout << "\n\n" << result << "\n\n";
+                        cout << "#############################################" << endl;
+                        cout << "###                                       ###" << endl;
+                        cout << "###       What is your " << i << " value?           ###" << endl;
+                        cout << "###                                       ###" << endl;
+                        cout << "#############################################" << endl;
+                        cout << "\t That will be: ";
+                        cin >> v;
+                        cout << "#############################################" << endl;
+                        cout << "###                                       ###" << endl;
+                        cout << "###       You selected: " << v << "                 ###" << endl;
+                        cout << "###                                       ###" << endl;
+                        cout << "###        Enter                          ###" << endl;
+                        cout << "###           + for Plus                  ###" << endl;
+                        cout << "###           - for Minus                 ###" << endl;
+                        cout << "###           * for douple                ###" << endl;
+                        cout << "###                                       ###" << endl;
+                        cout << "#############################################" << endl;
+                        cout << "\t I want to: ";
+                        cin >> dothis;
+                        //Calculating...
+                        int sum = result;
+                            if      (dothis == "+")
+                                {
+                                    result = pl(sum,v);
+                                }
+                            else if (dothis == "-")
+                                {
+                                    result = mi(sum,v);
+                                }
+                            else if (dothis == "*")
+                                {
+                                result = ma(sum,v);
+                                }
+                                else    {
 
-        cout << "Please log in" << endl;
+                        cout << "#############################################" << endl;
+                        cout << "###                                       ###" << endl;
+                        cout << "###        SOMETHING WENT WRONG           ###" << endl;
+                        cout << "###     ONE OF YOUR VALUE WAS SKIPPED     ###" << endl;
+                        cout << "###                                       ###" << endl;
+                        cout << "#############################################" << endl; }
+                        //RESULTS
+                        cout << "#############################################" << endl;
+                        cout << "###                                       ###" << endl;
+                        cout << "###         Result for now                ###" << endl;
+                        cout << "###           " << result << "                           ###" << endl;
+                        cout << "###                                       ###" << endl;
+                        cout << "#############################################" << endl;
 
-        cout << "How many value do you need to calculate?" << endl;
-        cout << "Select middle 2 - unlimited" << endl;
-        cin >> values;
-
-
-        for ( int i = 0; i < values; i++ )
- {
-        cout << "What is your " << i << " value?\n";
-        cin >> i;
-        int newresult = i;
-        int keepresult = newresult;
-        cout << keepresult << endl;
-        cout << "Prees - for Minus\n";
-        cout << "Prees + for Plus\n";
-        cout << "Prees * for for *\n\n";
-        cin >> dothis;
-        cout << "Result for now is" << keepresult << dothis << "\n\n";
+        }
 
 
-
+return 0;
 
  }
 
-
-
-
-
-
-
-
-
-
-        cout << 4 + 4 + 8 * 7 + 5 * 4 << endl;
-
-
-
-
-    return 0;
-}
